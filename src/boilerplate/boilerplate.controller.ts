f783c7ca6709f57a126d2d57346703de2d2487ec
Hugo De Grossi <contact@hugo-degrossi.fr>
2022-06-20 15:51:17 +0200

    TCP ready.

--- src/boilerplate/boilerplate.controller.ts
+++ src/boilerplate/boilerplate.controller.ts
@@ -2,19 +2,20 @@ import {Controller, Get, Post} from '@nestjs/common';
 import {BoilerplateService} from "./boilerplate.service";
 import {Boilerplate} from "../schemas/boilerplate.schema";
 import {CreateBoilerplateDto} from "../dto/create-boilerplate.dto";
+import {MessagePattern} from "@nestjs/microservices";
 
-@Controller('boilerplate')
+@Controller('test_db')
 export class BoilerplateController {
     constructor(private readonly boilerplateService: BoilerplateService) {}
 
     //@UseGuards(JwtGuard)
-    @Get()
+    @MessagePattern('getItems')
     getAll(): Promise<Boilerplate[]> {
         return this.boilerplateService.findAll();
     }
 
-    @Post()
-    postName(): Promise<Boilerplate> {
+    @MessagePattern('postItem')
+    postOne(): Promise<Boilerplate> {
         let bp:  Boilerplate = new CreateBoilerplateDto()
         bp.name = 'test'
         return this.boilerplateService.create(bp)
